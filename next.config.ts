28c79db33472c6b3f10cd1fe8f358c8ee4df784a
BURRA SRI PRAKASH <109139847+techhburner@users.noreply.github.com>
2026-08-29 22:25:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/scalorant",
+  assetPrefix: "/scalorant/",
   images: {
     unoptimized: true,
   },
